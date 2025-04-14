a9b6343acbb2c9a07800defac37d5aabf4d8000e
clover4to7 <lsh2716@naver.com>
2025-04-14 16:54:34 +0900

    campaign add

--- src/pages/Campaign.js
+++ src/pages/Campaign.js
@@ -19,6 +19,14 @@ const Campaign = () => {
             description: "광복절을 맞아 우리의 일상이 당연하지 않은 것임을 상기시키고, 독립운동가들의 희생과 공헌을 기억하는 캠페인입니다.",
             image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FoyA60%2Fbtst6jWFymF%2FEHl4Jlk4dcE69vhaw5Cp5k%2Fimg.png",
             link: "https://blog.uplus.co.kr/4547"
+        },
+        {
+            id: 3,
+            title: "독립운동가 후손 돕기",
+            company: "대한적십자사",
+            description: "'2021년 국가보훈대상자 생활실태조사'에 따르면 많은 독립유공자 후손들이 여전히 어려운 환경에서 살아가고 있습니다. 독립운동가 후손분들의 자긍심이 경제적 어려움에 무너지지 않도록 여러분의 따뜻한 도움의 손길을 부탁드립니다.",
+            image: "https://cdn.imweb.me/thumbnail/20250107/0969ad8ef17d8.jpg",
+            link: "https://redcross.campaignus.me/myfather"
         }
     ];
 
