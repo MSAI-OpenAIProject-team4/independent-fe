72ee22b71ef819df3f625c3dd9974b0bb3c524d5
clover4to7 <lsh2716@naver.com>
2025-04-15 16:43:05 +0900

    campaign add

--- src/pages/Campaign.js
+++ src/pages/Campaign.js
@@ -6,14 +6,22 @@ const Campaign = () => {
     const campaigns = [
         {
             id: 1,
-            title: "처음 입는 광복",
-            company: "빙그레",
-            description: "옥중 순국하여 마지막 모습이 죄수복으로 남은 독립운동가의 사진을 AI 기술로 복원하여, 독립운동 정신을 담은 빛나는 한복을 입은 영웅의 모습으로 재현하는 캠페인입니다.",
-            image: "https://www.bing.co.kr/upload/ckeditor/2024/08/08a8686d-68ed-4f20-aaa8-2f2c2cb531eb",
-            link: "https://www.bing.co.kr/news/news_announced_view?anno_idx=205"
+            title: "독립운동가 후손 돕기",
+            company: "대한적십자사",
+            description: "'2021년 국가보훈대상자 생활실태조사'에 따르면 많은 독립유공자 후손들이 여전히 어려운 환경에서 살아가고 있습니다. 독립운동가 후손분들의 자긍심이 경제적 어려움에 무너지지 않도록 여러분의 따뜻한 도움의 손길을 부탁드립니다.",
+            image: "https://cdn.imweb.me/thumbnail/20250107/0969ad8ef17d8.jpg",
+            link: "https://redcross.campaignus.me/myfather"
         },
         {
             id: 2,
+            title: "독립유공자 후손 주거개선 캠페인",
+            company: "한국해비타트",
+            description: "현재 정부의 적절한 지원을 받고 있는 독립유공자 후손들은 약 10%에 해당됩니다. 독립운동가 후손들이 편하게 여생을 보낼 수 있는 집과 환경이 필요합니다. 2017년부터 2022년까지 총 86가구의 주거환경을 개선했습니다.",
+            image: "https://habitat.or.kr/landing/2025/independence/images/bg_visual.png",
+            link: "https://habitat.or.kr/landing/2025/independence/"
+        },
+        {
+            id: 3,
             title: "당연하지 않은 일상",
             company: "LG유플러스",
             description: "광복절을 맞아 우리의 일상이 당연하지 않은 것임을 상기시키고, 독립운동가들의 희생과 공헌을 기억하는 캠페인입니다.",
@@ -21,12 +29,12 @@ const Campaign = () => {
             link: "https://blog.uplus.co.kr/4547"
         },
         {
-            id: 3,
-            title: "독립운동가 후손 돕기",
-            company: "대한적십자사",
-            description: "'2021년 국가보훈대상자 생활실태조사'에 따르면 많은 독립유공자 후손들이 여전히 어려운 환경에서 살아가고 있습니다. 독립운동가 후손분들의 자긍심이 경제적 어려움에 무너지지 않도록 여러분의 따뜻한 도움의 손길을 부탁드립니다.",
-            image: "https://cdn.imweb.me/thumbnail/20250107/0969ad8ef17d8.jpg",
-            link: "https://redcross.campaignus.me/myfather"
+            id: 4,
+            title: "처음 입는 광복",
+            company: "빙그레",
+            description: "옥중 순국하여 마지막 모습이 죄수복으로 남은 독립운동가의 사진을 AI 기술로 복원하여, 독립운동 정신을 담은 빛나는 한복을 입은 영웅의 모습으로 재현하는 캠페인입니다.",
+            image: "https://www.bing.co.kr/upload/ckeditor/2024/08/08a8686d-68ed-4f20-aaa8-2f2c2cb531eb",
+            link: "https://www.bing.co.kr/news/news_announced_view?anno_idx=205"
         }
     ];
 
